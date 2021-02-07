709785f95e8b3c0e568c9f1ff7bc3989ee72e174
fr33-tool <73328541+fr33-tool@users.noreply.github.com>
2021-02-07 13:28:51 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,31 @@
+apt install golang -y
+apt install git -y
+apt install python -y
+apt install python2 -y 
+pkg install root-repo
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+go get -u -v github.com/tomnomnom/assetfinder
+GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
+GO111MODULE=on go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+GO111MODULE=on go get -v github.com/projectdiscovery/httpx/cmd/httpx
+GO111MODULE=on go get -u -v github.com/lc/subjs
+apt install nmap -y
+go get github.com/tomnomnom/waybackurls
+go get -u -v github.com/lc/gau
+go get -u -v github.com/lukasikic/subzy
+git clone https://github.com/projectdiscovery/nuclei-templates
+pkg install findomain -y
+pkg install nmap -y
+go get -u -v github.com/lukasikic/subzy
+pkg install jq -y
+git clone https://github.com/nahamsec/lazys3
+GO111MODULE=on go get -v github.com/dwisiswant0/crlfuzz/cmd/crlfuzz
+git clone https://github.com/BountyStrike/Injectus
+GO111MODULE=on go get -v github.com/OWASP/Amass/v3/...
+pkg install wget -y
+git clone 
+https://github.com/nirsarkar/dirsearch-master
+pkg install dnsutils -y
+go get -u github.com/tomnomnom/qsreplace
+go get github.com/hakluke/hakcheckurl
