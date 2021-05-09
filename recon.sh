5c41c3cc48fa9d565965ff92097bf135c9c758d3
fr33-tool <73328541+fr33-tool@users.noreply.github.com>
2021-05-09 22:26:17 +0530

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -1,9 +1,13 @@
+if [[ -z $1 ]]
+then
+echo "you have not specified any target"
+else
 rm -rf /$HOME/recondata/$1
 mkdir /$HOME/recondata/$1
 cd /$HOME/recondata/$1
 subfinder -d $1 -all -o subfinder.txt
 assetfinder --subs-only $1 | sort -u >> assetfinder.txt
-findomain --quiet -t $1 -u findomain.txt 
+findomain --quiet -t $1 -u findomain.txt
 #usingcrt.sh at defferent levels
 curl -s https://crt.sh/?Identity=%.$1 | grep ">*.$1" | sed 's/<[/]*[TB][DR]>/\n/g' | grep -vE "<|^[\*]*[\.]*$1" | sort -u | awk 'NF' > crt.txt
 curl -s https://crt.sh/?Identity=%.%.$1 | grep ">*.$1" | sed 's/<[/]*[TB][DR]>/\n/g' | grep -vE "<|^[\*]*[\.]*$1" | sort -u | awk 'NF' > crt.txt
@@ -23,8 +27,9 @@ curl -s "https://jldc.me/anubis/subdomains/$1" | grep -Po "((http|https):\/\/)?(
 #using bufferover 
 curl -s https://dns.bufferover.run/dns?q=.$1 |jq -r .FDNS_A[] | sed -s 's/,/\n/g' |grep [a-z] >> $1.txt
 amass enum --passive -d $1 -o amass.txt
-cat $1.txt amass.txt subfinder.txt assetfinder.txt findomain.txt crt.txt | grep -F ".$1" | sed 's/\*\.//g' |sort -u >> passive.txt 
-rm -rf $1.txt subfinder.txt assetfinder.txt findomain.txt crt.txt amass.txt
+~/puredns/./puredns bruteforce ~/puredns/subdomains $1 -w $1
+cat $1.txt amass.txt subfinder.txt assetfinder.txt ~/puredns/$1 findomain.txt crt.txt | grep -F ".$1" | sed 's/\*\.//g' |sort -u >> passive.txt 
+rm $1.txt subfinder.txt assetfinder.txt ~/puredns/$1 findomain.txt crt.txt amass.txt
 
 #starting httprobe
 cat passive.txt | httprobe >> http.txt
@@ -32,20 +37,18 @@ cat passive.txt | httprobe >> http.txt
 nuclei -update-templates
 cat http.txt | nuclei -t /$HOME/nuclei-templates/ | tee nuclei.txt
 
-#getting all the available extensions.
-gau -subs $1 | egrep -i -E -o "\.{1}\w*$" | sort -su | tee ext
-
-#starting crlfuzz
-echo "started crlf injector"
-#crlfuzz -l http.txt -s | tee crlf.txt
-
 #starting subzy
 subzy -targets passive.txt -hide_fails | tee subvulns.txt
 
-#open redirect testing
-python3 /$HOME/Injectus/Injectus.py -f http.txt -op | tee openredirect.txt
+#staring robots files enumeration.
+cat http.txt >> hosts
+echo "/robots.txt" >> paths
+meg --verbose -s 200 paths hosts
+cat out/*/* | grep -i disallow | awk '{print $2}' | sort -u | tee robotspath.txt
 
-#scanning all the open ports.
-cat http.txt |grep -oP 'https://\K\S+'| sort -u >> nmap.txt
-nmap -iL nmap.txt -T5 | tee ports.txt
-
+#starting gau and hakrawler
+cat passive.txt |hakrawler -robots| tee roboturls.txt
+cat passive.txt |hakrawler -urls | awk '{print $2}' |tee urls.txt
+echo "started gau........"
+cat passive.txt |gau -subs |sort -u |tee -a urls.txt
+fi
